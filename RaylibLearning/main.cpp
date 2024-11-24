72d959f29b3ce217524fbbe042735ecef807dedc
NameUnderBedMonster <vodka.pivo.bff@gmail.com>
2024-11-24 22:51:59 +0200

    2dCameraTesting

--- RaylibLearning/main.cpp
+++ RaylibLearning/main.cpp
@@ -1,16 +1,91 @@
 #include "raylib.h"
+#define MAX_BUILDINGS   100
 
-int main() {
+typedef enum GameScreen { LOGO = 0, TITLE, GAMEPLAY, ENDING } GameScreen;
+
+int main(void)
+{
     const int screenWidth = 800;
-    const int screenHeight = 600;
-    InitWindow(screenWidth, screenHeight, "Raylib basic window");
+    const int screenHeight = 450;
+
+    Rectangle player = { 400, 280, 40, 40 };
+    Rectangle buildings[MAX_BUILDINGS] = { 0 };
+    Color buildColors[MAX_BUILDINGS] = { 0 };
+
+    int spacing = 0;
+
+    for (int i = 0; i < MAX_BUILDINGS; i++)
+    {
+        buildings[i].width = (float)GetRandomValue(50, 200);
+        buildings[i].height = (float)GetRandomValue(100, 800);
+        buildings[i].y = screenHeight - 130.0f - buildings[i].height;
+        buildings[i].x = -6000.0f + spacing;
+
+        spacing += (int)buildings[i].width;
+        buildColors[i] = Color{(unsigned char)GetRandomValue(200, 240),(unsigned char)GetRandomValue(200, 240),(unsigned char)GetRandomValue(200, 240),255};
+    }
+    
+
+    Camera2D camera = { 0 };
+    camera.target = Vector2{ player.x + 20.0f, player.y + 20.0f };
+    camera.offset = Vector2{ screenWidth / 2.0f, screenHeight / 2.0f };
+    camera.rotation = 0.0f;
+    camera.zoom = 1.0f;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    int framesCounter = 0;
     SetTargetFPS(60);
-    while (!WindowShouldClose()) {
+
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Player movement
+        if (IsKeyDown(KEY_RIGHT)) player.x += 2;
+        else if (IsKeyDown(KEY_LEFT)) player.x -= 2;
+
+        // Camera target follows player
+        camera.target = Vector2{ player.x + 20, player.y + 20 };
+
+        // Camera rotation controls
+        if (IsKeyDown(KEY_A)) camera.rotation--;
+        else if (IsKeyDown(KEY_S)) camera.rotation++;
+
+        // Limit camera rotation to 80 degrees (-40 to 40)
+        if (camera.rotation > 40) camera.rotation = 40;
+        else if (camera.rotation < -40) camera.rotation = -40;
+
+        // Camera zoom controls
+        camera.zoom += ((float)GetMouseWheelMove() * 0.05f);
+
+        if (camera.zoom > 3.0f) camera.zoom = 3.0f;
+        else if (camera.zoom < 0.1f) camera.zoom = 0.1f;
+
+        // Camera reset (zoom and rotation)
+        if (IsKeyPressed(KEY_R))
+        {
+            camera.zoom = 1.0f;
+            camera.rotation = 0.0f;
+        }
+
+
         BeginDrawing();
+
         ClearBackground(RAYWHITE);
-        DrawText("It works!", 20, 20, 20, BLACK);
+
+        
+        BeginMode2D(camera);
+
+        DrawRectangle(-6000, 320, 13000, 8000, DARKGRAY);
+
+        for (int i = 0; i < MAX_BUILDINGS; i++) DrawRectangleRec(buildings[i], buildColors[i]);
+
+        DrawRectangleRec(player, RED);
+
+        EndMode2D();
+
         EndDrawing();
     }
+
     CloseWindow();
     return 0;
 }
